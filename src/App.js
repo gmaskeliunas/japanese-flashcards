4abf5b8feb7417923ea9f70707980fa82ae1ab25
Gediminas Maskeliūnas <ged.mask@gmail.com>
2023-02-23 16:06:21 +0200

    Added site title with Helmet, corrected Kanji

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import CardContainer from "./components/CardContainer";
 import './App.css';
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <ChakraProvider>
-      <main>
-        <CardContainer />
-      </main>
-    </ChakraProvider>
+    <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Kanji Flashcards</title>
+        <link rel="canonical" href="https://gmaskeliunas.github.io/japanese-flashcards/" />
+        <meta name="description" content="Kanji flashcard app" />
+      </Helmet>
+      <ChakraProvider>
+        <main>
+          <CardContainer />
+        </main>
+      </ChakraProvider>
+    </div>
   );
 }
 
